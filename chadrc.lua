036ca3691dbb99ee3039784e9b0c9c7f20c19d46
David <zapeexp@gmail.com>
2023-04-16 19:08:51 -0500

    improved theme colors

--- chadrc.lua
+++ chadrc.lua
@@ -1,6 +1,16 @@
 ---@type ChadrcConfig
 local M = {}
-M.ui = { theme = "catppuccin" }
+M.ui = {
+  theme = "catppuccin",
+  changed_themes = {
+    catppuccin = {
+      base_30 = {
+        grey = "#808080", --line numbers color
+        grey_fg = "#6b6b6b", -- commentary color
+      },
+    },
+  },
+}
 M.plugins = "custom.plugins"
 M.mappings = require "custom.mappings"
 return M
